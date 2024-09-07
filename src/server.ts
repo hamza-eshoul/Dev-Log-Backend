cb96f9ff3c682c1e1963245b23cc5d85a10c1d0c
Hamza Eshoul <hamzaeshoul@Hamzas-MacBook-Pro-3.local>
2024-09-07 15:44:48 +0100

    changed prod config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
-const PORT: number = 3000;
+const PORT: number = parseInt(process.env.PORT || "3000");
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
