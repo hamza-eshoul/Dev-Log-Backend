3f6e0d45ec656065eaeac1310b91f55c0b75a1d2
Hamza Eshoul <hamzaeshoul@Hamzas-MacBook-Pro-3.local>
2024-09-14 11:02:08 +0100

    reinforced the security of the API

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { Express } from "express";
 import cors from "cors";
 import blogPostsRouter from "./routes/blogPosts";
 import { apiKeyAuth } from "./middleware/apiKeyAuth";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 // Initialize app
 const app: Express = express();
@@ -12,6 +14,14 @@ app.use(express.json());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  })
+);
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests, please try again after 15 minutes",
   })
 );
 
